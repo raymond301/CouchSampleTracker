acdcc0a8d8d320f2419e7a55d3cd1691303633ac
Raymond <raymond.moore301@gmail.com>
2016-01-19 09:27:03 -0600

    collapse glob headers into 1 object

--- db/migrate/20151106211839_create_demographic_globs.rb
+++ db/migrate/20151106211839_create_demographic_globs.rb
@@ -2,8 +2,8 @@ class CreateDemographicGlobs < ActiveRecord::Migration
   def change
     create_table :demographic_globs do |t|
       t.references :sample, index: true
-      t.references :demographic_glob_header, index: true
-      t.text :delim_text
+      t.binary :delim_header
+      t.binary :delim_text
     end
   end
 end
