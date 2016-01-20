6deb43ab72897c33496c641035c80d37f2319dfa
Raymond <raymond.moore301@gmail.com>
2016-01-20 08:46:48 -0600

    more normalization

--- lib/tasks/normalizer.rb
+++ lib/tasks/normalizer.rb
@@ -2,7 +2,38 @@ require 'pp'
 
 ## Modify all samples, attributes specific
 Sample.all.each do |s|
-  s.case_control.downcase.capitalize!
+  if s.case_control =~ /case/i
+    s.case_control = "Case"
+  elsif s.case_control =~ /control/i
+    s.case_control = "Control"
+  end
+
+
+  if ! s.gender.blank? then s.gender.downcase.capitalize! end
+  if ! s.species.blank? then s.species.downcase.capitalize! end
+  if ! s.tissue_source.blank? then s.tissue_source.downcase.capitalize! end
+
+  br
+  if s.cancer_type == "BR"
+    s.cancer_type = "Breast"
+  elsif s.cancer_type == "br"
+      s.cancer_type = "Breast"
+  elsif s.cancer_type == "breast_cancer"
+    s.cancer_type = "Breast"
+  elsif s.cancer_type == "OV"
+    s.cancer_type = "Ovarian"
+  elsif s.cancer_type == "BR/OV"
+    s.cancer_type = "Breast/Ovarian"
+  elsif s.cancer_type == "TNBC"
+    s.cancer_type = "Triple Negative Breast"
+  else
+    s.cancer_type.capitalize!
+  end
 
   s.save!
 end
+
+
+
+#### need to renormalize the project designation
+### gepar contains BCFRs, need to remove
